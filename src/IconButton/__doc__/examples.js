f0789a42f9553a81de9e99e58b01b87ae4f20202
Andrey Polishchuk <a.polishchuk@rambler-co.ru>
2017-03-09 19:15:52 +0300

    RAMBLERUI-14 update icons in <IconButton /> docs

--- src/IconButton/__doc__/examples.js
+++ src/IconButton/__doc__/examples.js
@@ -1,6 +1,7 @@
 import IconButton from 'rambler-ui/IconButton'
 import React from 'react'
 import RamblerMailIcon from 'rambler-ui/icons/services/RamblerMailIcon'
+import { AddIcon, RemoveIcon, UndoIcon, PhotoCameraIcon } from 'rambler-ui/icons/forms'
 import { ApplyTheme } from 'rambler-ui/theme'
 
 export default function IconButtonExample() {
@@ -9,16 +10,12 @@ export default function IconButtonExample() {
 
       <ApplyTheme>
         <div>
-          <div>
-            <IconButton style={{margin: 20}}><RamblerMailIcon /></IconButton>
-            <IconButton style={{margin: 20}} type="secondary"><RamblerMailIcon /></IconButton>
-            <IconButton style={{margin: 20}} type="outline"><RamblerMailIcon /></IconButton>
-          </div>
-          <div>
-            <IconButton style={{margin: 20}} type="secondary" size="small"><RamblerMailIcon /></IconButton>
-            <IconButton style={{margin: 20}} type="flat" size="small"><RamblerMailIcon /></IconButton>
-            <IconButton style={{margin: 20}} type="danger" size="small"><RamblerMailIcon /></IconButton>
-          </div>
+          <IconButton style={{margin: 20}}><RamblerMailIcon /></IconButton>
+          <IconButton style={{margin: 20}} type="secondary"><UndoIcon /></IconButton>
+          <IconButton style={{margin: 20}} type="outline"><RamblerMailIcon /></IconButton>
+          <IconButton style={{margin: 20}} type="secondary" size="small"><PhotoCameraIcon /></IconButton>
+          <IconButton style={{margin: 20}} type="flat" size="small"><AddIcon /></IconButton>
+          <IconButton style={{margin: 20}} type="danger" size="small"><RemoveIcon /></IconButton>
         </div>
       </ApplyTheme>
 
