5075dfcfb3498e308f57ef782bad7523ae4976d9
a.vorobyev <a.vorobyev@rambler-co.ru>
2017-09-08 16:07:28 +0300

    добавил иконку поиска

---
+++ src/icons/forms/SearchIcon.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import SvgIcon from '../SvgIcon'
+import pure from 'recompose/pure'
+
+@pure
+export default class SearchIcon extends Component {
+
+  static displayName = 'SearchIcon'
+
+  render() {
+    return (
+      <SvgIcon { ...this.props } viewBox="0 0 12 12">
+        <path fill="#FFF" fillRule="nonzero" d="M8.168 7.107l3.007 3.008a.499.499 0 0 1-.004.714l-.342.342a.501.501 0 0 1-.714.004L7.107 8.168a4.5 4.5 0 1 1 1.06-1.06zM4.5 7.5a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+      </SvgIcon>
+    )
+  }
+
+}
