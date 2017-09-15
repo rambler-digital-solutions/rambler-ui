7e2278d1bc95d1ad9df318a98a280f93c0bf42a3
ordyakov <a.ordyakov@rambler-co.ru>
2017-09-15 15:30:32 +0300

    Tabs test update

--- src/Tabs/__test__/Tabs.test.js
+++ src/Tabs/__test__/Tabs.test.js
@@ -1,6 +1,8 @@
 import { Tabs, TabsItem } from '../../Tabs'
 import { ApplyTheme } from '../../theme'
-import { mount, getWrapperNode } from '../../utils/test-utils'
+import { mount, getStyles, getWrapperNode } from '../../utils/test-utils'
+import theme from '../../theme/base'
+import { normalize as nc } from '../../utils/colors'
 
 import React from 'react'
 
@@ -8,10 +10,13 @@ const applyTheme = (children) => (
   <ApplyTheme>{ children }</ApplyTheme>
 )
 
+const transparentColor = 'rgba(0, 0, 0, 0)'
+
 describe('<Tabs />', () => {
   const tabsProps = {
     id: 'tabsId',
-    className: 'tabs'
+    className: 'tabs',
+    onChange: () => {}
   }
   const linkProps = {
     id: 'link',
@@ -56,11 +61,11 @@ describe('<Tabs />', () => {
 
   it('TabsItem content, element type and attributes', () => {
     const wrapper = mount(applyTheme(
-      <Tabs>
-        <TabsItem {...linkProps} />
-        <TabsItem {...buttonProps} />
-        <TabsItem {...containerProps} />
-      </Tabs>
+      <div>
+        <TabsItem {...linkProps} className="test" />
+        <TabsItem {...buttonProps} size="small" />
+        <TabsItem {...containerProps} size="medium" />
+      </div>
     ))
 
     const link = getWrapperNode(wrapper.find('#' + linkProps.id))
@@ -68,22 +73,24 @@ describe('<Tabs />', () => {
     const container = getWrapperNode(wrapper.find('#' + containerProps.id))
 
     expect(link.tagName.toUpperCase()).toEqual('A')
+    expect(link.classList.contains('test')).toEqual(true)
     expect(link.getAttribute('href')).toEqual(linkProps.href)
-    expect(link.getAttribute('aria-selected')).toEqual('false')
     expect(link.getAttribute('aria-disabled')).toEqual('false')
+    expect(link.className.includes('size-small')).toEqual(true)
+    expect(link.className.includes('isEnabled')).toEqual(true)
     expect(link.innerHTML).toEqual(linkProps.children)
 
     expect(button.tagName.toUpperCase()).toEqual('BUTTON')
-    expect(button.getAttribute('aria-selected')).toEqual('false')
     expect(button.disabled).toEqual(false)
+    expect(link.className.includes('size-small')).toEqual(true)
     expect(button.innerHTML).toEqual(buttonProps.children)
 
     expect(container.tagName.toUpperCase()).toEqual('DIV')
     expect(container.getAttribute('title')).toEqual(containerProps.title)
     expect(container.getAttribute('data-own')).toEqual('own')
     expect(container.getAttribute('data-test').includes('isSelected')).toEqual(true)
-    expect(container.getAttribute('aria-selected')).toEqual('false')
     expect(container.getAttribute('aria-disabled')).toEqual('false')
+    expect(container.className.includes('size-medium')).toEqual(true)
     expect(container.innerHTML).toEqual(containerProps.children)
   })
 
@@ -102,22 +109,16 @@ describe('<Tabs />', () => {
     const container = getWrapperNode(wrapper.find('#' + containerProps.id))
     const notSelected = getWrapperNode(wrapper.find('#notSelected'))
 
+    expect(link.className.includes('size-small')).toEqual(true)
     expect(link.className.includes('isSelected')).toEqual(true)
-    expect(link.getAttribute('aria-selected')).toEqual('true')
-
     expect(button.className.includes('isSelected')).toEqual(true)
-    expect(button.getAttribute('aria-selected')).toEqual('true')
-
     expect(container.className.includes('isSelected')).toEqual(true)
-    expect(container.getAttribute('aria-selected')).toEqual('true')
-
     expect(notSelected.className.includes('isSelected')).toEqual(false)
-    expect(notSelected.getAttribute('aria-selected')).toEqual('false')
   })
 
   it('disabled Tabs and TabsItem attributes', () => {
     const wrapper = mount(applyTheme(
-      <Tabs disabled>
+      <Tabs disabled size="medium">
         <TabsItem {...linkProps} />
         <TabsItem {...buttonProps} />
         <TabsItem {...containerProps} />
@@ -131,13 +132,140 @@ describe('<Tabs />', () => {
 
     expect(tabs.className.includes('isDisabled')).toEqual(true)
 
+    expect(link.className.includes('size-medium')).toEqual(true)
     expect(link.className.includes('isDisabled')).toEqual(true)
     expect(link.getAttribute('aria-disabled')).toEqual('true')
+    expect(link.getAttribute('tabindex')).toEqual('-1')
 
     expect(button.className.includes('isDisabled')).toEqual(true)
     expect(button.disabled).toEqual(true)
 
     expect(container.className.includes('isDisabled')).toEqual(true)
     expect(container.getAttribute('aria-disabled')).toEqual('true')
+    expect(link.getAttribute('tabindex')).toEqual('-1')
+  })
+
+  it('TabsItem onPress handler', () => {
+    let event, value
+    const props = {
+      value: 'test',
+      onPress: (e, val) => {
+        event = e
+        value = val
+      }
+    }
+
+    const wrapper = mount(applyTheme(
+      <TabsItem {...props} />
+    ))
+
+    wrapper.simulate('click')
+    expect(event.type).toEqual('click')
+    expect(value).toEqual(props.value)
+  })
+
+  it('Tabs onChange handler', () => {
+    let event, value
+    const props = {
+      value: linkProps.id,
+      onChange: (e, val) => {
+        event = e
+        value = val
+      }
+    }
+    const wrapper = mount(applyTheme(
+      <Tabs {...props}>
+        <TabsItem {...linkProps} value={linkProps.id} />
+        <TabsItem {...buttonProps} value={buttonProps.id} />
+        <TabsItem {...containerProps} value={containerProps.id} />
+      </Tabs>
+    ))
+    const linkEl = getWrapperNode(wrapper.find('#' + linkProps.id))
+    const button = wrapper.find('#' + buttonProps.id)
+    const buttonEl = getWrapperNode(button)
+    const container = wrapper.find('#' + containerProps.id)
+    const containerEl = getWrapperNode(container)
+
+    expect(linkEl.className.includes('isSelected')).toEqual(true)
+    expect(buttonEl.className.includes('isSelected')).toEqual(false)
+    expect(containerEl.className.includes('isSelected')).toEqual(false)
+    button.simulate('click')
+    expect(event.type).toEqual('click')
+    expect(value).toEqual(buttonProps.id)
+    expect(linkEl.className.includes('isSelected')).toEqual(false)
+    expect(buttonEl.className.includes('isSelected')).toEqual(true)
+    expect(containerEl.className.includes('isSelected')).toEqual(false)
+    container.simulate('click')
+    expect(linkEl.className.includes('isSelected')).toEqual(false)
+    expect(buttonEl.className.includes('isSelected')).toEqual(false)
+    expect(containerEl.className.includes('isSelected')).toEqual(true)
+  })
+
+  it('TabsItem styles', () => {
+    const wrapper = mount(applyTheme(
+      <TabsItem {...linkProps} className="test" />
+    ))
+
+    const link = getStyles(wrapper)
+    expect(link['border-bottom-width']).toEqual(theme.tabs.borderWidth + 'px')
+    expect(link['border-bottom-style']).toEqual('solid')
+    expect(link['border-bottom-color']).toEqual(transparentColor)
+    expect(nc(link.color)).toEqual(nc(theme.tabs.colors.default.text))
+  })
+
+  it('selected TabsItem styles', () => {
+    const wrapper = mount(applyTheme(
+      <TabsItem {...buttonProps} isSelected />
+    ))
+
+    const button = getStyles(wrapper)
+    expect(nc(button.color)).toEqual(nc(theme.tabs.colors.selected.text))
+    expect(nc(button['border-bottom-color'])).toEqual(nc(theme.tabs.colors.selected.text))
+  })
+
+  it('disabled TabsItem styles', () => {
+    const wrapper = mount(applyTheme(
+      <TabsItem {...containerProps} disabled />
+    ))
+
+    const container = getStyles(wrapper)
+    expect(nc(container.color)).toEqual(nc(theme.tabs.colors.disabled.text))
+    expect(container['border-bottom-color']).toEqual(transparentColor)
+  })
+
+  it('selected disabled TabsItem styles', () => {
+    const wrapper = mount(applyTheme(
+      <TabsItem {...linkProps} disabled isSelected />
+    ))
+
+    const link = getStyles(wrapper)
+    expect(nc(link.color)).toEqual(nc(theme.tabs.colors.disabled.text))
+    expect(nc(link['border-bottom-color'])).toEqual(nc(theme.tabs.colors.disabledSelected.border))
+  })
+
+  it('Tabs styles', () => {
+    const wrapper = mount(applyTheme(
+      <Tabs value="selected">
+        <TabsItem {...linkProps} value="selected"/>
+        <TabsItem {...buttonProps} />
+        <TabsItem {...containerProps} />
+      </Tabs>
+    ))
+
+    const tabs = getStyles(wrapper)
+    const link = getStyles(wrapper.find('#' + linkProps.id))
+    const button = getStyles(wrapper.find('#' + buttonProps.id))
+    const container = getStyles(wrapper.find('#' + containerProps.id))
+
+    expect(tabs['padding-left']).toEqual(theme.tabs.sidePadding + 'px')
+    expect(tabs['padding-right']).toEqual(theme.tabs.sidePadding + 'px')
+    expect(tabs.display.includes('inline-flex')).toEqual(true)
+
+    expect(link['margin-left']).toEqual(0 + 'px')
+    expect(button['margin-left']).toEqual(theme.tabs.betweenMargin + 'px')
+    expect(container['margin-left']).toEqual(theme.tabs.betweenMargin + 'px')
+    expect(link['margin-right']).toEqual(0 + 'px')
+    expect(button['margin-right']).toEqual(0 + 'px')
+    expect(container['margin-right']).toEqual(0 + 'px')
   })
 })
