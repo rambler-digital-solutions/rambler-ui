1abb08dcc236594083766ca7f7195ef2d3b9bfea
Andrey Polishchuk <a.polishchuk@rambler-co.ru>
2017-12-01 14:01:02 +0300

    Refactor apply theme

---
+++ src/theme/__test__/index.test.js
@@ -0,0 +1,152 @@
+import React from 'react'
+import classnames from 'classnames'
+import {ApplyTheme, injectSheet, createJss, createSheetsRegistry, createGenerateClassName} from '../'
+import {mount, getNodeStyles} from '../../utils/test-utils'
+import {normalize as nc} from '../../utils/colors'
+
+const getTheme = color => ({button: {color}})
+
+const Button = injectSheet(({button}) => ({button}))(
+  function Button ({classes, className}) {
+    return (
+      <button className={classnames(classes.button,  className)} />
+    )
+  }
+)
+
+describe('<ApplyTheme />', () => {
+
+  it('should pass theme', () => {
+    mount(
+      <ApplyTheme theme={getTheme('#ffffff')}>
+        <Button className='apply-theme' />
+      </ApplyTheme>
+    )
+
+    const button = document.querySelector('.apply-theme')
+    expect(getNodeStyles(button).color).toBe(nc('#ffffff'))
+  })
+
+  it('should merge nested theme', () => {
+    mount(
+      <ApplyTheme theme={getTheme('#ffffff')}>
+        <div>
+          <Button className='apply-nested' />
+          <ApplyTheme theme={getTheme('#000000')}>
+            <Button className='apply-nested' />
+          </ApplyTheme>
+        </div>
+      </ApplyTheme>
+    )
+
+    const buttons = document.querySelectorAll('.apply-nested')
+    expect(getNodeStyles(buttons[0]).color).toBe(nc('#ffffff'))
+    expect(getNodeStyles(buttons[1]).color).toBe(nc('#000000'))
+    expect(buttons[0].className).not.toBe(buttons[1].className)
+  })
+
+  it('should isolate multiple providers', () => {
+    mount(
+      <div>
+        <ApplyTheme theme={getTheme('#ffffff')}>
+          <Button className='apply-isolate' />
+        </ApplyTheme>
+        <ApplyTheme theme={getTheme('#000000')}>
+          <Button className='apply-isolate' />
+        </ApplyTheme>
+      </div>
+    )
+
+    const buttons = document.querySelectorAll('.apply-isolate')
+    expect(getNodeStyles(buttons[0]).color).toBe(nc('#ffffff'))
+    expect(getNodeStyles(buttons[1]).color).toBe(nc('#000000'))
+    expect(buttons[0].className).not.toBe(buttons[1].className)
+  })
+
+  it('should use custom jss, sheets registry', () => {
+    const jss = createJss()
+    const sheetsRegistry = createSheetsRegistry()
+
+    mount(
+      <ApplyTheme
+        theme={getTheme('#ffffff')}
+        jss={jss}
+        sheetsRegistry={sheetsRegistry}>
+        <div>
+          <Button />
+          <ApplyTheme theme={getTheme('#000000')}>
+            <Button />
+          </ApplyTheme>
+        </div>
+      </ApplyTheme>
+    )
+
+    const classPrefix = sheetsRegistry.registry[0].classes.button.slice(0, -2)
+
+    expect(sheetsRegistry.toString()).toBe(
+      `.${classPrefix}-1 {\n` +
+      '  color: #ffffff;\n' +
+      '}\n' +
+      `.${classPrefix}-2 {\n` +
+      '  color: #000000;\n' +
+      '}'
+    )
+  })
+
+  it('should use multiple custom jss, sheets registry', () => {
+    const generateClassName = createGenerateClassName()
+    const jss = createJss()
+    const sheetsRegistry = createSheetsRegistry()
+    const nestedJss = createJss()
+    const nestedSheetsRegistry = createSheetsRegistry()
+    const separateJss = createJss()
+    const separateSheetsRegistry = createSheetsRegistry()
+
+    mount(
+      <div>
+        <ApplyTheme
+          theme={getTheme('#ffffff')}
+          jss={jss}
+          sheetsRegistry={sheetsRegistry}
+          generateClassName={generateClassName}>
+          <div>
+            <Button />
+            <ApplyTheme
+              theme={getTheme('#000000')}
+              jss={nestedJss}
+              sheetsRegistry={nestedSheetsRegistry}>
+              <Button />
+            </ApplyTheme>
+          </div>
+        </ApplyTheme>
+        <ApplyTheme
+          theme={getTheme('#777777')}
+          jss={separateJss}
+          sheetsRegistry={separateSheetsRegistry}
+          generateClassName={generateClassName}>
+          <Button />
+        </ApplyTheme>
+      </div>
+    )
+
+    const classPrefix = sheetsRegistry.registry[0].classes.button.slice(0, -2)
+
+    expect(sheetsRegistry.toString()).toBe(
+      `.${classPrefix}-1 {\n` +
+      '  color: #ffffff;\n' +
+      '}'
+    )
+
+    expect(nestedSheetsRegistry.toString()).toBe(
+      `.${classPrefix}-2 {\n` +
+      '  color: #000000;\n' +
+      '}'
+    )
+
+    expect(separateSheetsRegistry.toString()).toBe(
+      `.${classPrefix}-3 {\n` +
+      '  color: #777777;\n' +
+      '}'
+    )
+  })
+})
--- src/theme/index.js
+++ src/theme/index.js
@@ -1,63 +1,29 @@
 import merge from 'lodash/merge'
-import omit from 'lodash/omit'
-import {create as createOriginalJss, SheetsRegistry} from 'jss'
-import createGenerateClassName from 'jss/lib/utils/createGenerateClassName'
+import {create as originalCreateJss} from 'jss'
+import originalInjectSheet, {createTheming, JssProvider, SheetsRegistry, createGenerateClassName} from 'react-jss'
 import preset from 'jss-preset-default'
-import originalInjectSheet, {createTheming, JssProvider} from 'react-jss'
-import * as jssNs from 'react-jss/lib/ns'
-import mapProps from 'recompose/mapProps'
 import compose from 'recompose/compose'
-import withContext from 'recompose/withContext'
-import getContext from 'recompose/getContext'
 import withPropsOnChange from 'recompose/withPropsOnChange'
-import PropTypes from 'prop-types'
 import base from './base'
 
 const RAMBLER_UI_THEME = '__RAMBLER_UI_THEME__'
-const RAMBLER_UI_JSS = '__RAMBLER_UI_JSS__'
-const RAMBLER_UI_SHEETS_REGISTRY = '__RAMBLER_UI_SHEETS_REGISTRY__'
-const RAMBLER_UI_JSS_PROVIDER_ID = '__RAMBLER_UI_JSS_PROVIDER_ID__'
-
 const theming = createTheming(RAMBLER_UI_THEME)
+const {ThemeProvider} = theming
 
-export const createJss = () => {
-  // Иначе при использовании нескольких ApplyTheme могут быть коллизии
-  const generateClassName = createGenerateClassName()
-  return createOriginalJss({
-    ...preset(),
-    createGenerateClassName: () => generateClassName
-  })
-}
+export {createGenerateClassName}
+export const createJss = (options) => originalCreateJss(preset(options))
 export const createSheetsRegistry = () => new SheetsRegistry()
-
-export const globalJss = createJss()
-export const globalSheetsRegistry = createSheetsRegistry()
-
 
 /**
  * Делаем совместимым с нашим компонентом ApplyTheme
  */
 export const ApplyTheme = compose(
-  getContext({
-    [RAMBLER_UI_JSS]: PropTypes.object,
-    [RAMBLER_UI_SHEETS_REGISTRY]: PropTypes.object
-  }),
   withPropsOnChange(
     () => false,
-    (props) => {
+    ({theme = base}) => {
       // Создаем свойства один раз при создании компонента
       let resultTheme, currTheme, currParentTheme
-      // https://github.com/cssinjs/react-jss/issues/133
-      const providerId = Math.random()
-      const theme = props.theme || base
-      const sheetsRegistry = props.sheetsRegistry || props[RAMBLER_UI_SHEETS_REGISTRY] || globalSheetsRegistry
-      const jss = props.jss || props[RAMBLER_UI_JSS] || globalJss
-      const shouldAddJssProvider = !props[RAMBLER_UI_JSS]
       return {
-        jss,
-        sheetsRegistry,
-        shouldAddJssProvider,
-        [RAMBLER_UI_JSS_PROVIDER_ID]: providerId,
         getResultTheme: (parentTheme) => {
           if (currTheme !== theme || currParentTheme !== parentTheme) {
             resultTheme = merge({}, parentTheme, theme)
@@ -68,35 +34,20 @@ export const ApplyTheme = compose(
         }
       }
     }
-  ),
-  withContext(
-    {
-      [RAMBLER_UI_JSS]: PropTypes.object,
-      [RAMBLER_UI_SHEETS_REGISTRY]: PropTypes.object,
-      [RAMBLER_UI_JSS_PROVIDER_ID]: PropTypes.number
-    },
-    props => ({
-      [RAMBLER_UI_JSS]: props.jss,
-      [RAMBLER_UI_SHEETS_REGISTRY]: props.sheetsRegistry
-    })
   )
-)(({
-  jss, sheetsRegistry, getResultTheme, shouldAddJssProvider, children
-}) => {
-  const provider = <theming.ThemeProvider theme={getResultTheme} children={children} />
-  if (!shouldAddJssProvider)
+)(({jss, sheetsRegistry, getResultTheme, generateClassName, children}) => {
+  const provider = (
+    <ThemeProvider theme={getResultTheme}>
+      {children}
+    </ThemeProvider>
+  )
+  if (!jss)
     return provider
-  return <JssProvider jss={jss} registry={sheetsRegistry}>{provider}</JssProvider>
+  return (
+    <JssProvider jss={jss} registry={sheetsRegistry} generateClassName={generateClassName}>
+      {provider}
+    </JssProvider>
+  )
 })
 
-export const injectSheet = styles => compose(
-  getContext({
-    [RAMBLER_UI_JSS_PROVIDER_ID]: PropTypes.number
-  }),
-  withContext(
-    {[jssNs.providerId]: PropTypes.number},
-    props => ({[jssNs.providerId]: props[RAMBLER_UI_JSS_PROVIDER_ID]})
-  ),
-  originalInjectSheet(styles, {theming}),
-  mapProps(props => omit(props, RAMBLER_UI_JSS_PROVIDER_ID))
-)
+export const injectSheet = styles => originalInjectSheet(styles, {theming})
