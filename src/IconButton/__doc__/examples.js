a8fd6788c838878b174fbf86847563131e17bafe
Alexey Ordyakov <a.ordyakov@rambler-co.ru>
2018-03-21 14:28:20 +0300

    Update example for IconButton

--- src/IconButton/__doc__/examples.js
+++ src/IconButton/__doc__/examples.js
@@ -1,8 +1,7 @@
 import IconButton from 'rambler-ui/IconButton'
 import React from 'react'
 import RamblerMailIcon from 'rambler-ui/icons/services/RamblerMailIcon'
-import {MailruIcon, GoogleIcon} from 'rambler-ui/icons/profiles'
-import { PhotoCameraIcon } from 'rambler-ui/icons/forms'
+import GoogleIcon from 'rambler-ui/icons/profiles/GoogleIcon'
 import { ApplyTheme } from 'rambler-ui/theme'
 
 const types = ['primary', 'danger', 'secondary', 'outline', 'flat']
@@ -16,13 +15,9 @@ export default function IconButtonExample() {
             <IconButton style={{margin: 15}} type={type}><RamblerMailIcon /></IconButton>
             <IconButton style={{margin: 15}} type={type} loading={true}><RamblerMailIcon /></IconButton>
             <IconButton style={{margin: 15}} type={type} disabled={true}><RamblerMailIcon /></IconButton>
-            <IconButton style={{margin: 15}} type={type} size="medium"><MailruIcon /></IconButton>
-            <IconButton style={{margin: 15}} type={type} size="medium" disabled={true}><MailruIcon /></IconButton>
             <IconButton style={{margin: 15}} type={type} size="small"><GoogleIcon /></IconButton>
             <IconButton style={{margin: 15}} type={type} size="small" disabled={true}><GoogleIcon /></IconButton>
             <IconButton style={{margin: 15}} type={type} size="small" loading={true}><GoogleIcon /></IconButton>
-            <IconButton style={{margin: 15}} type={type} size={23}><PhotoCameraIcon /></IconButton>
-            <IconButton style={{margin: 15}} type={type} size={23} loading={true}><PhotoCameraIcon /></IconButton>
           </div>
         ))}
       </div>
