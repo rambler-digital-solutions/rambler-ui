508e1b20dc90adb819e7fcf1afd429c499915029
Andrey Polishchuk <a.polishchuk@rambler-co.ru>
2018-04-06 22:51:02 +0300

    Fix checking the user agent in SSR

--- src/utils/browser.js
+++ src/utils/browser.js
@@ -1,4 +1,5 @@
-const ua = window && window.navigator.userAgent
+const ua = typeof window !== 'undefined' &&
+  window.navigator && window.navigator.userAgent
 
 export const windowsPhone = ua && /IEMobile|Windows Phone/i.test(ua)
 export const ios = ua && /iPhone|iPad|iPod/i.test(ua) && !windowsPhone
