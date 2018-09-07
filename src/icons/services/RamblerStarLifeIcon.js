add25504fe6d57a33e612f0de91c10a74754ca4e
Vorobyev Andrey <a.vorobyev@rambler-co.ru>
2018-09-07 13:42:44 +0300

    добавил новую иконку для сервиса "шоу-бизнес"

---
+++ src/icons/services/RamblerStarLifeIcon.js
@@ -0,0 +1,15 @@
+import React, { PureComponent } from 'react'
+import SvgIcon from '../SvgIcon'
+
+export default class RamblerStarLifeIcon extends PureComponent {
+
+  static displayName = 'RamblerStarLifeIcon'
+
+  render() {
+    return (
+      <SvgIcon { ...this.props } viewBox="0 0 20 20">
+        <path id="a" d="M11 14.25v-.5a.5.5 0 0 0-.5-.5h-7a.5.5 0 0 0-.5.5v.5a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 .5-.5m6-4v-.5a.5.5 0 0 0-.5-.5h-13a.5.5 0 0 0-.5.5v.5a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5m0-4v-.5a.5.5 0 0 0-.5-.5h-13a.5.5 0 0 0-.5.5v.5a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5M20 0v20M0 20V0"/>
+      </SvgIcon>
+    )
+  }
+}
