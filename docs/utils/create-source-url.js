6cdad32fc3f4e4c7918ccd519558aa01425d48fb
Andrey Polishchuk <a.polishchuk@rambler-co.ru>
2018-11-06 14:41:59 +0300

    Fix docs url to repo

--- docs/utils/create-source-url.js
+++ docs/utils/create-source-url.js
@@ -7,7 +7,9 @@ export default function createSourceUrl(pathname) {
   const path = pathname.substring(1).split(/\//)
   const prefix = path[0]
   if (prefix !== 'components')
-    return `${config.repoLink}tree/${config.branch}/docs${pathname}/index.md`
+    return `${config.repoLink}tree/${
+      config.branch
+    }/docs/pages${pathname}/index.md`
   const filename = path.pop()
   if (filename == null || prefix === filename) return
   return `${config.repoLink}tree/${config.branch}/src/${filename}`
