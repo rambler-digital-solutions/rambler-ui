0ebd7a4fb092e623ea78787bf71b581b99a7e27d
Andrey Polishchuk <a.polishchuk@rambler-co.ru>
2019-04-05 17:56:45 +0300

    update avatar example

--- docs/pages/components/Avatar/examples.js
+++ docs/pages/components/Avatar/examples.js
@@ -4,41 +4,45 @@ import Avatar from 'rambler-ui/Avatar'
 export default function AvatarExample() {
   const props = {
     style: {
-      marginLeft: 10
+      marginRight: 10,
+      marginBottom: 10
     },
     src: '//api.adorable.io/avatars/face/eyes4/nose5/mouth7/8e8895/'
   }
 
   return (
-    <div>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        marginBottom: 10
+      }}>
+      <div style={{marginRight: 20}}>
+        <h4>shape: circle</h4>
+        <Avatar {...props} profileType="facebook" />
+        <Avatar {...props} size={36} profileType="facebook" />
+        <Avatar {...props} size={75} href="#/components/Button" />
+      </div>
+
+      <div style={{marginRight: 20}}>
+        <h4>shape: rounded</h4>
+        <Avatar {...props} shape="rounded" profileType="facebook" />
+        <Avatar {...props} shape="rounded" size={25} profileType="facebook" />
+        <Avatar
+          {...props}
+          shape="rounded"
+          size={75}
+          profileType="instagram"
+          iconBackgroundColor="#f3f4f7"
+          container={<a href="#/components/Button" />}
+        />
+      </div>
+
       <div>
-        <div style={{display: 'inline-block', width: '33%'}}>
-          Circle
-          <Avatar {...props} profileType="facebook" />
-          <Avatar {...props} size={36} profileType="facebook" />
-          <Avatar {...props} size={100} href="#/components/Button" />
-        </div>
-
-        <div style={{display: 'inline-block', width: '33%'}}>
-          Rounded
-          <Avatar {...props} shape="rounded" profileType="facebook" />
-          <Avatar {...props} shape="rounded" size={25} profileType="facebook" />
-          <Avatar
-            {...props}
-            shape="rounded"
-            size={100}
-            profileType="instagram"
-            iconBackgroundColor="#f3f4f7"
-            container={<a href="#/components/Button" />}
-          />
-        </div>
-
-        <div style={{display: 'inline-block', width: '33%'}}>
-          Square
-          <Avatar {...props} shape="square" profileType="livejournal" />
-          <Avatar {...props} shape="square" size={25} />
-          <Avatar {...props} shape="square" size={100} />
-        </div>
+        <h4>shape: square</h4>
+        <Avatar {...props} shape="square" profileType="livejournal" />
+        <Avatar {...props} shape="square" size={25} />
+        <Avatar {...props} shape="square" size={75} />
       </div>
     </div>
   )
