acd89f84da8000c6b946c6646654f562619d08cc
Игорь Звягинцев <i.zvyagintsev@rambler-co.ru>
2019-06-19 13:07:30 +0300

    Тип для Typography

--- index.d.ts
+++ index.d.ts
@@ -8,3 +8,4 @@ type TVariation = "regular" | "awesome"
 type TSize = "small" | "medium"
 type TType = "primary" | "secondary" | "outline" | "flat" | "danger"
 type TCalVariation = "service" | "media"
+type TypographyType = 'h1' | 'h2' | 'h3' | 'text' | 'quote' | 'epigraph' | 'source' | 'timestamp' | 'description' | 'galleryDescription' | 'photoSource' | 'list'
