6294fb176e49448c28c74b720176670508aafc02
Daniil <dzabrotskiy@yandex.ru>
2019-09-30 13:38:54 +0300

    resolve conflict in Tooltip

--- src/Tooltip/Tooltip.js
+++ src/Tooltip/Tooltip.js
@@ -373,8 +373,7 @@ export default class Tooltip extends PureComponent {
   //   if (this.props.isOpened) this.show()
   // }
 
-  onMouseEnter = event => {
-    event.stopPropagation()
+  onMouseEnter = () => {
     this.show()
   }
 
