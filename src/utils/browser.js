6bf9dff2ae2d4e231d521c47e482716b1d2514b7
Daniil <dzabrotskiy@yandex.ru>
2019-12-18 00:34:44 +0300

    [Slider] add IE and Edge support

--- src/utils/browser.js
+++ src/utils/browser.js
@@ -6,3 +6,7 @@ const ua =
 export const windowsPhone = ua && /IEMobile|Windows Phone/i.test(ua)
 export const ios = ua && /iPhone|iPad|iPod/i.test(ua) && !windowsPhone
 export const android = ua && /Android/i.test(ua) && !windowsPhone
+
+export const isIE =
+  navigator.userAgent.indexOf('MSIE') !== -1 || !!document.documentMode === true
+export const isEdge = !isIE && !!window.StyleMedia
