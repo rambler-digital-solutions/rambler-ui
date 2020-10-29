29e43aa59279a482ba6198be94b090cba01879ee
m.tsipotan <m.tsipotan@MAC812.local>
2020-10-29 18:20:44 +0300

    fix: add nodeRef to SvgIconProps

--- src/SvgIcon/SvgIcon.d.ts
+++ src/SvgIcon/SvgIcon.d.ts
@@ -4,6 +4,7 @@ import {Size} from '../'
 export interface SvgIconProps extends SVGAttributes<SVGElement> {
   children?: ReactNode | ((size: number) => ReactNode)
   size?: string | number | Size | 'large'
+  nodeRef?: (ref: HTMLElement) => void
 }
 
 declare const SvgIcon: FC<SvgIconProps>
