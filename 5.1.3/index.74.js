e55e3b4ce7bce9a898efc40e4c3b4774c00b4a58
Polishchuk Andrey <a.polishchuk@rambler-co.ru>
2020-11-25 13:51:02 +0000

    Update gh-pages

---
+++ 5.1.3/index.74.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[74],{146:function(e,n,t){"use strict";t.r(n),t.d(n,"default",(function(){return i}));var a=t(0),r=t.n(a),c=t(59),o=t(280),u=r.a.createElement("div",null,r.a.createElement(o.default,{container:r.a.createElement(c.b,{to:"/home"})},"Кнопка-ссылка"));function i(){return u}}}]);
