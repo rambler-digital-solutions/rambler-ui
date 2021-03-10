9d783de8dbbeb5b402b5cc9e1f20d99c44ac8606
Daniil <dzabrotskiy@yandex.ru>
2021-03-10 19:18:09 +0300

    add controls type

--- src/theme/index.d.ts
+++ src/theme/index.d.ts
@@ -13,12 +13,31 @@ export {
 export {default as ThemeProvider} from './ThemeProvider'
 export * from './ThemeProvider'
 
+export type Control = {
+  default: string
+  disabled: string
+  icon: string
+  background: string
+  lightBackground: string
+  activeBackground: string
+  outline: string
+  fieldOutline: string
+  iconBackground: string
+  placeholder: string
+  fieldIcon: string
+  disabledText: string
+}
+
+export type Controls = {
+  grey: Control
+}
+
 export type Colors = {
   arrowBlueDark: string
   black: string
   blueDark: string
   blueLight: string
-  controls: object
+  controls: Controls
   danger: string
   dark: string
   fullDark: string
