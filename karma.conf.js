f9f93c39f678829593271587ab963751f1384db3
Andrey Polishchuk <a.polishchuk@rambler-co.ru>
2021-05-26 17:56:19 +0300

    5.3.6

--- karma.conf.js
+++ karma.conf.js
@@ -88,7 +88,7 @@ module.exports = config =>
       ...(component === '**' && {
         check: {
           global: {
-            branches: 45
+            branches: 44
           }
         }
       })
