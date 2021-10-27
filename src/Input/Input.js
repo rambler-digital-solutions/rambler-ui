419f59185f20a0b8d1a98fe2151fe7b2769e729a
Andrey Polishchuk <a.polishchuk@rambler-co.ru>
2021-10-27 15:20:39 +0300

    5.5.1

--- src/Input/Input.js
+++ src/Input/Input.js
@@ -46,7 +46,6 @@ const styles = theme => ({
     boxShadow: 'none',
     border: '0 solid',
     transition: `all ${theme.field.animationDuration}ms ease`,
-    borderRadius: theme.field.borderRadius,
     ...ifMobile({
       fontWeight: theme.field.mobile.fontWeight,
       letterSpacing: theme.field.mobile.letterSpacing
